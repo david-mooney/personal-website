52c0bc449b81b6b9836b1490da437de49ecbefdf
dmooney <dmooney@hubpsot.com>
2019-04-17 17:43:59 +0100

    Added react and parcel

---
+++ src/scripts/Main.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  font-size: 1.2rem;
+  color: salmon;
+`;
+
+const Main = () => {
+  return (
+    <Wrapper>Coming Soon...</Wrapper>
+  );
+};
+
+Main.propTypes = {};
+
+export default(Main);
---
+++ src/scripts/index.jsx
@@ -0,0 +1,8 @@
+import React from 'React';
+import { render } from 'react-dom';
+import Main from './Main';
+import './../styles/index.scss';
+
+const root = document.getElementById('root');
+
+render(<Main />, root);
